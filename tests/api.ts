9873d57dc2246293b918d9d60a56735fbd997733
Randy Hunt <randy@rhunt.com>
2024-09-14 21:51:49 -0400

    break out the mock server

--- tests/api.ts
+++ tests/api.ts
@@ -1,86 +1,11 @@
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
-import { http, HttpResponse} from 'msw'
-import { setupServer } from 'msw/node'
-import { XMLParser, XMLBuilder } from 'fast-xml-parser'
+import server from './mock-server'
 import Kunaki from '../src/index'
 import type { Auth } from '../types/api'
 import type { Order, Product, ShippingDestination } from '../types/models'
 
 const API = suite('Kunaki API')
-const parser = new XMLParser()
-const builder = new XMLBuilder({ ignoreAttributes: true })
-
-const apiError = (code: number, text: string): HttpResponse => {
-  return HttpResponse.xml(builder.build({ Response: { ErrorCode: code, ErrorText: text }}))
-}
-
-const server = setupServer(
-  http.post('https://unittest/XMLService.ASP', async ({ request, params, cookies }) => {
-    const data = await request.text()
-    const req = parser.parse(data)
-    if (req.ShippingOptions) {
-      const so = req.ShippingOptions
-      const p: Product[] = [].concat(so.Product)
-      if (!so.Country) return apiError(2, "Country element is missing")
-      if (!so.State_Province) return apiError(4, "State_Province element is missing")
-      if (!so.PostalCode) return apiError(6, "PostalCode element is missing")
-      if (!p || p.length === 0 || !p[0].ProductId) return apiError(8, "ProductId element is missing")
-      if (!p[0].Quantity) return apiError(10, "Quantity element is missing")
-
-      const xml = builder.build({
-        Response: {
-          ErrorCode: 0,
-          ErrorText: "success",
-          Option: [
-            { Description: "USPS First Class", DeliveryTime: "2-5 days", Price: "1.29" },
-            { Description: "UPS Ground", DeliveryTime: "2-4 days", Price: "7.90" },
-            { Description: "Fedex 3-day", DeliveryTime: "3 days", Price: "8.24" },
-          ]
-        }
-      })
-      return HttpResponse.xml(xml)
-    }
-
-    if (req.Order) {
-      const o = req.Order
-      const p: Product[] = [].concat(o.Product)
-      if (!o.Country) return apiError(2, "Country element is missing")
-      if (!o.State_Province) return apiError(4, "State_Province element is missing")
-      if (!o.PostalCode) return apiError(6, "PostalCode element is missing")
-      if (!p || p.length === 0 || !p[0].ProductId) return apiError(8, "ProductId element is missing")
-      if (!p[0].Quantity) return apiError(10, "Quantity element is missing")
-      if (!o.UserId || !o.Password) return apiError(12, "UserId and/or password is not valid")
-
-      const xml = builder.build({
-        Response: {
-          ErrorCode: 0,
-          ErrorText: "success",
-          OrderId: "567129"
-        }
-      })
-      return HttpResponse.xml(xml)
-    }
-
-    if (req.OrderStatus) {
-      const os = req.OrderStatus
-      if (!os.UserId || !os.Password) return apiError(12, "UserId and/or password is not valid")
-      if (!os.OrderId) return apiError(20, "OrderId element is missing")
-      const xml = builder.build({
-        Response: {
-          ErrorCode: 0,
-          ErrorText: "success",
-          OrderId: "567129",
-          OrderStatus: "processing",
-          TrackingType: "Fedex",
-          TrackingId: "XIU897Z9876"
-        }
-      })
-      return HttpResponse.xml(xml)
-    }
-    return HttpResponse.error()
-  })
-)
 
 API.before(() => server.listen())
 API.after(() => server.close())
